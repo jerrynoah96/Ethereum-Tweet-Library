3f9d34d9e66a3991827fda8645670607b8a5b3b4
Jeremiah <newcreationjerry96@gmail.com>
2020-07-27 16:12:33 +0100

    profile suggestion alert

---
+++ index.js
@@ -0,0 +1,4 @@
+// close profile suggestion alert
+document.querySelector('.close-alert').addEventListener('click', ()=>{
+  document.querySelector('.profile__suggest-alert').style.display = 'none';
+})
